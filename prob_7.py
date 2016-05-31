fb11df9864678f051ed787e74781da838d072d4b
Douma242 <danieldouma@hotmail.es>
2016-05-31 09:32:11 -0600

    se completo el problema 7

--- prob_7.py
+++ prob_7.py
@@ -1,9 +1,16 @@
 def prob_7(l1):
-	l1=[4]
+	miset=[4, 7]
 	n=1
 	res=0
 	while res<1000:
 		n=n+1
-		res=4*n
-		l1.append(res)
-	return l1
+		res=miset[0]*n
+		miset.append(res)
+	n2=1
+	res2=0
+	while res2<994:
+		n2=n2+1
+		res2=miset[1]*n2
+		miset.append(res2)
+	miset.sort()
+	return miset
---
+++ pruebas.py
@@ -0,0 +1,3 @@
+import prob_7
+l1=[4]
+print (prob_7.prob_7(l1))
